dc720ef463dabb938a21011132d37e6115624fc9
aceytok <ace.manuyag@edu.sait.ca>
2023-09-14 16:25:35 -0600

    Adding app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+let x = 4;
+let y = 3;
+
+let z = x + y;
+
+const greeting = "Hello ";
+const name = "Ace";
+
+let msg = greeting + name;
+
+console.log(z);
+console.log(msg);
+console.log("CPRG306");
+
